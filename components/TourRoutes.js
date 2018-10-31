398fd6d397eb6ee61dd0eb64d6b8818e15c0ce99
Leonard <leothepro555@gmail.com>
2018-10-31 15:06:38 +0800

    Added a latitude and longtitude displayer here for possible future use.

--- components/TourRoutes.js
+++ components/TourRoutes.js
@@ -4,10 +4,41 @@ import { Text } from 'native-base';
 import NavigationBar from './NavigationBar';
 
 export default class TourRoutes extends Component {
+  state = {
+    latitude: 0,
+    longitude: 0
+  }
+
+  componentWillUnmount = () => {
+    this.mounted = false;
+  };
+
+  updateLocation(){
+    this.mounted = true;
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    navigator.geolocation.getCurrentPosition(
+    ({coords}) => {
+      if(this.mounted){
+        this.setState({
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        })
+      }
+    },
+    () => console.error(`ERROR(${err.code}): ${err.message}`),
+    options);
+  }
   render() {
+    this.updateLocation();
     return (
       <NavigationBar {...this.props}>
         <Text>Tour Routes</Text>
+        <Text> Geolocation running </Text>
+        <Text>Latitude: {this.state.latitude}; Longitude: {this.state.longitude}</Text>
       </NavigationBar>
     )
   }
