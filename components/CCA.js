4c9ef183f38c18d70b6b4326bc61598bf590c095
Leonard <leothepro555@gmail.com>
2018-10-31 15:02:05 +0800

    Copied what was done in Curriculum.js to make the stuff clickable

--- components/CCA.js
+++ components/CCA.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { Text, List, ListItem } from 'native-base';
+import { Text, List, ListItem, Left, Right, Icon, View } from 'native-base';
+import { createStackNavigator } from 'react-navigation';
 
 import NavigationBar from './NavigationBar';
 
-export default class CCA extends Component {
+class ListView extends Component {
   render() {
     let sportsCCAs = ['Badminton', 'Basketball', 'Canoeing', 'Cross Country', 'Fencing', 'Floorall', 'Golf', 'Judo'];
-    let sportsDisplay = sportsCCAs.map(e => (
-      <ListItem key={e}>
-        <Text>{e}</Text>
+    let sportsDisplay = sportsCCAs.map(cca => (
+      <ListItem
+        key={cca}
+        button onPress={() => this.props.navigation.navigate({
+          routeName: 'CCAType',
+          params: {ccaName: cca}
+        })}
+      >
+        <Left>
+          <Text>{cca}</Text>
+        </Left>
+        <Right>
+          <Icon name="arrow-forward" />
+        </Right>
       </ListItem>
     ))
     return (
@@ -23,3 +35,32 @@ export default class CCA extends Component {
     )
   }
 }
+
+
+class CCAType extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('ccaName', 'CCA Name'),
+    };
+  };
+
+  render() {
+    const {getParam} = this.props.navigation;
+    let ccaName = getParam('ccaName', 'CCA Name');
+    return (
+      <View style={{margin:10}}>
+        <Text>{ccaName} is a cca.</Text>
+      </View>
+    )
+  }
+}
+
+export default createStackNavigator({
+  CCA: {
+    screen: ListView,
+    navigationOptions: ({
+      header: null
+    })
+  },
+  CCAType: CCAType,
+});
