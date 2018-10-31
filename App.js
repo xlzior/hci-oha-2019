57a59c27882c17387715c1cf36e9327b1640a122
Leonard <leothepro555@gmail.com>
2018-10-31 15:00:40 +0800

    Some margins added

--- App.js
+++ App.js
@@ -14,6 +14,24 @@ import Curriculum from './components/Curriculum';
 import FAQs from './components/FAQs';
 
 class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontLoaded: false
+    }
+  }
+
+  async componentDidMount() {
+	  Expo.Font.loadAsync({
+		'Roboto':require('native-base/Fonts/Roboto.ttf'),
+		'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+	})
+	.then(() => this.setState({ fontLoaded: true}))
+	.catch(() => this.setState({ fontLoaded: true}));
+	  
+  }
+
   generateButtonGrid(buttons) {
     let buttonsList = buttons.map(route => {
       return (
@@ -39,25 +57,31 @@ class HomeScreen extends React.Component {
     )
   }
   render() {
+    let {fontLoaded} = this.state;
+    if (!fontLoaded) return <View style={styles.center}><Text>Loading...</Text></View>
     let ohaDisplay = this.generateButtonGrid(['Schedule', 'Map', 'Tour Routes', 'Redemption'])
     let hciDisplay = this.generateButtonGrid(['About', 'Curriculum', 'CCAs', 'FAQs'])
 
     return (
       <NavigationBar {...this.props}>
-        <Image
-          source={require('./images/mosaic.png')}
-          resizeMode='contain'
-          style={{height: 250, alignSelf: 'center'}}
-        />
-        <H1 style={styles.H1}>Hwa Chong Institution</H1>
-        <H1 style={styles.H1}>College Open House 2019</H1>
-        <Text>
-          Welcome to the Open House 2019, where you join us in creating this colourful and spectacular masterpiece, one that we call home, as Hwa Chongians.
-        </Text>
-        <H2 style={styles.H2}>Open House 2019</H2>
-        {ohaDisplay}
-        <H2 style={styles.H2}>Hwa Chong Institution</H2>
-        {hciDisplay}
+        <View style={styles.mainContainer}>
+          <Image
+            source={require('./images/mosaic.png')}
+            resizeMode='contain'
+            style={{height: 250, alignSelf: 'center', marginBottom: 10}}
+          />
+          <H1 style={styles.H1}>Hwa Chong Institution</H1>
+          <H1 style={styles.H1}>College Open House 2019</H1>
+          <View style={{marginLeft:5,marginRight:5,marginTop:10}}>
+            <Text style={{textAlign: 'center'}}>
+              Welcome to the Open House 2019, where you join us in creating this colourful and spectacular masterpiece, one that we call home, as Hwa Chongians.
+            </Text>
+          </View>
+          <H2 style={styles.H2}>Open House 2019</H2>
+          {ohaDisplay}
+          <H2 style={styles.H2}>Hwa Chong Institution</H2>
+          {hciDisplay}
+        </View>
       </NavigationBar>
   );
   }
@@ -75,7 +99,7 @@ const RootDrawer = createDrawerNavigator({
   FAQs: FAQs,
 },
 {
-  initialRouteName: 'Curriculum'
+  initialRouteName: 'Home'
 });
 
 export default class App extends React.Component {
@@ -87,6 +111,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   button: {
     margin: 5,
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  mainContainer: {
+    margin:10,
     flex: 1,
     display: 'flex',
     justifyContent: 'center'
