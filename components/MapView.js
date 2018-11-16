c7cd6ab23afcd808a4305acf7b6e1634320401f3
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2018-11-16 10:50:52 +0800

    Initial MapView commit
    MapView displays a Maps component which is initialised at the school area and has a map overlayed over the area

--- components/MapView.js
+++ components/MapView.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { Text } from 'native-base';
+import { Text, View } from 'native-base';
+import MapView, { Marker, Overlay } from 'react-native-maps';
+import Dimensions from 'Dimensions';
+const {width, height} = Dimensions.get('window');
 
 import NavigationBar from './NavigationBar';
 
-export default class MapView extends Component {
+export default class MapViewContainer extends Component {
   render() {
     return (
       <NavigationBar {...this.props}>
-        <Text>Map</Text>
+        <MapView
+          style={{flex: 1, height: height-50}}
+          initialRegion={{
+            latitude: 1.327139, 
+            longitude: 103.804468,
+            latitudeDelta: 0.021,
+            longitudeDelta: 0.008
+          }}
+          showsUserLocation={true}
+        >
+        <Overlay
+          image={{uri: "https://static.wixstatic.com/media/89ebb0_69ec8ee9e22d477aaadfcf8a8c6ab779~mv2_d_4266_3200_s_4_2.png"}}
+          bounds={[[1.329238,103.800667], [1.323872, 103.806640]]}
+        />
+        </MapView>
       </NavigationBar>
     )
   }
 }
+
+// bounds={[[1.329238,103.800667], [1.323872, 103.806640]]}
