4c59113fad78519442883a739b5a4f6393d4e489
wenjunlye <30801910+wenjunlye@users.noreply.github.com>
2018-12-02 21:06:23 +0800

    Review code and clean up
    Standardise to 2 spaces
    Fix bug in FullWidthImage
    Add React Native Hyperlink as a dependency

--- components/FullWidthImage.js
+++ components/FullWidthImage.js
@@ -1,44 +1,45 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { View } from 'native-base';
+
 export default class FullWidthImage extends Component {
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.state = {
-            width: 0,
-            height: 0
-        };
-    }
+		this.state = {
+			width: 0,
+			height: 0
+		};
+	}
 
-    _onLayout(event) {
-        const containerWidth = event.nativeEvent.layout.width;
+	_onLayout(event) {
+    const containerWidth = event.nativeEvent.layout.width;
 
-        if (this.props.ratio) {
-            this.setState({
-                width: containerWidth,
-                height: containerWidth * this.props.ratio
-            });
-        } else {
-            Image.getSize(this.props.source, (width, height) => {
-                this.setState({
-                    width: containerWidth,
-                    height: containerWidth * height / width
-                });
-            });
-        }
-    }
+		if (this.props.ratio) {
+			this.setState({
+				width: containerWidth,
+				height: containerWidth * this.props.ratio
+			});
+		} else {
+			Image.getSize(this.props.source.uri, (width, height) => {
+				this.setState({
+					width: containerWidth,
+					height: containerWidth * height / width
+				});
+			});
+		}
+	}
 
-    render() {
-        return (
-            <View onLayout={this._onLayout.bind(this)}>
-                <Image
-                    source={this.props.source}
-                    style={{
-                        width: this.state.width,
-                        height: this.state.height
-                    }} />
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View onLayout={e => this._onLayout(e)}>
+				<Image
+					source={this.props.source}
+					style={{
+						width: this.state.width,
+						height: this.state.height
+					}} />
+			</View>
+		);
+	}
 }
